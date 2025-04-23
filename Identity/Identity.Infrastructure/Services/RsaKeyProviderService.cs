1065fd2fed1f5f0819803d8915f1145024e7ddd7
DESKTOP-IQVN00D\siyoyo <yoyo.mitrais@gmail.com>
2025-04-24 02:39:03 +0700

    Adding JWT Asymmetric Key Approach with automatic key rotation:
    -JwtGeneratorService.cs
    -RsaKeyProvider
    
    Baseline project: https://github.com/evgenirusev/.NET-Domain-Driven-Design-Template

---
+++ Identity/Identity.Infrastructure/Services/RsaKeyProviderService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+
+namespace Identity.Infrastructure.Services
+{
+    public class RsaKeyProviderService : IRsaKeyProvider
+    {
+        private const string SignatureUse = "sig";
+
+        private RSA rsa;
+        private JsonWebKey jsonWebKey;
+        private string keyId;
+
+        public RsaKeyProviderService(IOptions<ApplicationSettings> appSettings)
+        {
+            var rotationInterval = TimeSpan.FromSeconds(appSettings.Value.KeyRotationIntervalSeconds);
+
+            GenerateKeys();
+
+            new Timer(_ => GenerateKeys(), null, rotationInterval, rotationInterval);
+        }
+
+        private void GenerateKeys()
+        {
+            rsa?.Dispose();
+            rsa = RSA.Create(2048);
+            keyId = Guid.NewGuid().ToString();
+
+            var rsaSecurityKey = new RsaSecurityKey(rsa.ExportParameters(false))
+            {
+                KeyId = keyId
+            };
+
+            jsonWebKey = JsonWebKeyConverter.ConvertFromRSASecurityKey(rsaSecurityKey);
+            jsonWebKey.Use = SignatureUse;
+        }
+
+        public RSA GetPrivateKey() => rsa;
+
+        public JsonWebKey GetPublicJwk() => jsonWebKey;
+    }
+}
